05b5b8c9ee780d4e789a1f515d16d3c506c54fdc
megha55 <megha55@hotmail.com>
2012-09-29 23:39:31 +0530

    Generate C++ basic files

---
+++ cpp-files.sh
@@ -0,0 +1,58 @@
+# Shell script to create basic files for a c++ program
+
+clear
+
+main_file=main.cpp
+
+echo "Generating basic files for your simple C++ program."
+
+echo "Enter the name of program"
+read program_name
+
+mkdir $program_name
+cd $program_name
+
+#----------------------------------------------------------------
+#                       Header file
+#----------------------------------------------------------------
+
+touch $program_name.h
+echo "// File contains class and variables declaration"\
+    " and header files\n\n"\
+    "#include <iostream> \n"\
+    "using namespace std; \n\n"\
+    "//Class declaration\n"\
+    "class $program_name \n"\
+    "{ \n"\
+    "\tprivate: \n"\
+    "\t\t// Data members\n\n"\
+    "\tpublic: \n"\
+    "\t\t// Member functions\n"\
+    "}; \n"\
+    > $program_name.h
+
+#----------------------------------------------------------------
+#                   Class members definition file
+#----------------------------------------------------------------
+
+touch $program_name.cc
+echo "// File contains class and variables definition \n\n"\
+    "#include \"$program_name.h\" \n"\
+    > $program_name.cc
+
+#----------------------------------------------------------------
+#                       Main functiin file
+#----------------------------------------------------------------
+
+touch $main_file
+echo "// File contains main function \n\n"\
+    "#include \"$program_name.h\" \n\n"\
+    "int main() \n"\
+    "{ \n" \
+    "\t// Create class objects  \n"\
+    "\t$program_name ob; \n\n"\
+    "} \n"\
+     > $main_file
+     
+echo "\n"Your three basic files are created in $program_name.
+
